f33d5caea9f8344c1e98d88d23f485b1e5c473ca
MCUITE09 <jason0975243297@gmail.com>
2017-03-13 18:36:14 +0800

    Add files via upload

---
+++ lab03-2.ino
@@ -0,0 +1,16 @@
+double Analogin=0;
+int i;
+double Vin;
+void setup() {
+ Serial.begin(9600);
+ pinMode(A0,INPUT);
+}
+
+void loop() {
+ for (i=0;i<=5;i++){
+Analogin = 0.7* Analogin + 0.3* analogRead(A0);
+Vin = Analogin * 5 / 1024;
+Serial.println(Vin);
+delay(500);
+ }
+}
