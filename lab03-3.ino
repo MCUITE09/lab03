a085b36e181d231da18381a512a010eabe0d4f10
MCUITE09 <jason0975243297@gmail.com>
2017-03-13 20:38:24 +0800

    Add files via upload

---
+++ lab03-3.ino
@@ -0,0 +1,23 @@
+double ResisterValue;
+double sensorValue;
+double sensorVoltage;
+double R1;
+int i;
+void setup() {
+ Serial.begin(9600);
+ pinMode(A1,INPUT);
+ ResisterValue = 1023;
+}
+
+void loop() {
+ for (i=0;i<=5;i++)
+sensorValue = 0.7* sensorValue + 0.3* analogRead(A1);
+sensorVoltage = sensorValue * 5 / 1024;
+//sensorVoltage= 5 * R1 /900000;
+R1 = sensorVoltage * 1091000/ 5;
+Serial.println(sensorValue);
+Serial.println(sensorVoltage);
+Serial.println(R1);
+delay(1000);
+ 
+}
