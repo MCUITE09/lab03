75fc5fd33531509772e55d34ea5d6b1f2f8c3cf1
MCUITE09 <jason0975243297@gmail.com>
2017-03-13 17:56:37 +0800

    Add files via upload

---
+++ lab03-1.ino
@@ -0,0 +1,23 @@
+const byte LED = 13;
+void setup() {
+ pinMode(13,OUTPUT);
+Serial.begin(9600);
+   Serial.println("Hello World");
+}
+void loop() {
+  int val;0
+  
+if(Serial.available()){
+  val = Serial.read();
+  switch(val){
+  case '0':
+  digitalWrite(LED,LOW);
+  Serial.println("LED OFF");
+  break;
+  case'1':
+  digitalWrite(LED,HIGH);  
+   Serial.println("LED ON"); 
+  break;
+  }
+}
+}
